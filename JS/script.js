cc4f59910e3041aa733ec6b46dcc8bf937b3faf8
Pablo <pablonietoperez369@gmail.com>
2024-08-19 03:25:08 -0500

    Cambios al 90

---
+++ JS/script.js
@@ -0,0 +1,89 @@
+$(function(){
+    setTimeout(function(){
+        $("#header").addClass("headerAnimation");
+    }, 350);
+});
+
+$(function(){
+    $(".touch-add-info-one").click(function(e){
+        e.preventDefault();
+        $(".add-info-one").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-two").click(function(e){
+        e.preventDefault();
+        $(".add-info-two").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-three").click(function(e){
+        e.preventDefault();
+        $(".add-info-three").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-four").click(function(e){
+        e.preventDefault();
+        $(".add-info-four").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-five").click(function(e){
+        e.preventDefault();
+        $(".add-info-five").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-six").click(function(e){
+        e.preventDefault();
+        $(".add-info-six").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-seven").click(function(e){
+        e.preventDefault();
+        $(".add-info-seven").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-eight").click(function(e){
+        e.preventDefault();
+        $(".add-info-eight").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-nine").click(function(e){
+        e.preventDefault();
+        $(".add-info-nine").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-ten").click(function(e){
+        e.preventDefault();
+        $(".add-info-ten").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-eleven").click(function(e){
+        e.preventDefault();
+        $(".add-info-eleven").toggleClass("add-info-get");
+    });
+});
+
+$(function(){
+    $(".touch-add-info-twelve").click(function(e){
+        e.preventDefault();
+        $(".add-info-twelve").toggleClass("add-info-get");
+    });
+});
